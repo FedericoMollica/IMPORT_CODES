7e105b3e4ce498560bcdbf49804a7c0623490498
FEDERICO MOLLICA <103517325+FedericoMollica@users.noreply.github.com>
2022-09-21 15:04:24 +0200

    Add files via upload
    CODE TO OBTAIN AN EXCEL WITH ALL THE DICOM INFORMATION FROM CTs SAVED IN ONE FOLDER

---
+++ DICOM_TAG_ANALYSIS_FM_v1.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[50]:
+
+
+#AUTHOR: FEDERICO MOLLICA 2022
+#SCRIPT: DICOM_TAG_ANALYSIS_FM_v1
+
+#DESCRIPTION: 
+#code to obtain an excel file containing all the dicom info from
+#all the CTs present in one folder
+
+#PROS:
+#the file will be auto saved:'date of today' +'_DICOM_TAG_ANALYSIS_FM.xlsx'
+
+from pydicom import dcmread
+from dicom_csv import join_tree
+import pandas as pd
+import os
+from datetime import date
+
+today = date.today()
+dt = today.strftime('%Y%m%d')
+#code to set the date of today in a format YYMMDD
+
+df = join_tree("/Users/048252/Downloads/ASPEN_TEST", relative=False, verbose=1)
+#df = join_tree("ADD FOLDER PATH", relative=False, verbose=1)
+df = df.drop_duplicates(subset = ['SeriesInstanceUID'], keep= 'first')
+#codes to extract all the dicom info and keep only the unique series id
+
+df.to_excel('/Users/048252/Downloads/' + dt + '_DICOM_TAG_ANALYSIS_FM.xlsx')
+#df.to_excel('ADD FOLDER PATH' + dt + '_DICOM_TAG_ANALYSIS_FM.xlsx')
+#code to save the info as excel file:
+#date of today + '_DICOM_TAG_ANALYSIS_FM.xlsx'
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
