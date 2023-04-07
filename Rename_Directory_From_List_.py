769cea1053907b18e9eed8cac2c0ee6efa534284
MIGS <103517325+FedericoMollica@users.noreply.github.com>
2023-04-07 14:32:12 +0200

    Update Rename_Directory_From_List_.py

--- Rename_Directory_From_List_.py
+++ Rename_Directory_From_List_.py
@@ -26,3 +26,27 @@ for f in file_list:
             os.rename(f, Name[p] + '.png')
 
     
+    
+#RENAMING A LIST OF FOLDERS USING A AN EXCEL COLUMN
+    
+import os
+import random
+import pandas as pd
+
+os.chdir(r'D:\Federico_Local_BUP\PCD_2023_test')
+file_list = os.listdir(r'D:\Federico_Local_BUP\PCD_2023_test')
+file_list.sort(reverse=False)
+#file_list.pop(0)
+df = pd.read_excel(r'C:\Users\048252\Downloads\folder_names.xlsx', header = 0)
+
+Name = []
+p = -1
+
+for i in df['A1']:
+    Name.append(i)
+    
+for f in file_list:
+    p += 1
+    if p <= 5: 
+        if p != Name:
+            os.rename(f, Name[p])
